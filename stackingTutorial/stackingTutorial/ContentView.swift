5f98fab84f141c9164db65de593de2c2f247768f
TyrelJJackson <tyreljjackson@gmail.com>
2020-06-20 20:12:04 -0600

    project completed

--- stackingTutorial/stackingTutorial/ContentView.swift
+++ stackingTutorial/stackingTutorial/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Text("")
+                .frame(width: 100, height: 50)
+                .background(Color.green.opacity(0.3))
+            Text("")
+            .frame(width: 100, height: 50)
+            .background(Color.red.opacity(0.3))
+        }
     }
 }
 
