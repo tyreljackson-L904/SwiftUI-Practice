b2d24b9c5f5c78f1d72d96ee0088ddeee36102e4
TyrelJJackson <tyreljjackson@gmail.com>
2020-06-20 18:48:10 -0600

    initialized tutorial project

---
+++ stackingTutorial/stackingTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stackingTutorial
+//
+//  Created by Tyrel Jackson on 6/20/20.
+//  Copyright © 2020 Tyrel Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
