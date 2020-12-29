02166751daa6b8a02bbac12702fc2d997c6c5de3
Tyrel Jackson <tyreljackson904@gmail.com>
2020-12-29 14:40:52 -0700

    init commit

---
+++ Weather/Weather/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Tyrel Jackson on 12/29/20.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello World")
+        
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tyrel Jackson on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
